665057009d1ce65740d89fff8a145b6611b4905e
Kevin Qiu <kzqiu01@gmail.com>
2024-04-26 13:21:11 -0400

    Added initial version of online sysid.

---
+++ examples/online_sysid/hello.cc
@@ -0,0 +1,26 @@
+#include <gflags/gflags.h>
+
+#include <drake/common/text_logging_flags.h>
+
+namespace drake {
+namespace examples {
+namespace sysid {
+namespace {
+
+DEFINE_string(test, "Kevin", "Test string.");
+
+void DoMain() {
+	drake::log()->info("Hello " + FLAGS_test);
+}
+
+} // namespace
+} // namespace sysid
+} // namespace examples
+} // namespace drake
+
+int main(int argc, char *argv[]) {
+	gflags::SetUsageMessage("Hello example.");
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
+	drake::examples::sysid::DoMain();
+	return 0;
+}
